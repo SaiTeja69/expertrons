a88ff140dae90b8b9a333c0fae03f0e651bb947b
SaiTeja69 <54733232+SaiTeja69@users.noreply.github.com>
2020-07-16 23:50:17 +0530

    Update Expertrons_Sai_Sakilam_Question3.py

--- ExpertronsHiringSolution-Sai/ProgrammingSolutions/Expertrons_Sai_Sakilam_Question3.py
+++ ExpertronsHiringSolution-Sai/ProgrammingSolutions/Expertrons_Sai_Sakilam_Question3.py
@@ -1,4 +1,4 @@
-arr = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]] 
+arr = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]  #arr=[[input(),float(input())] for _ in range(0,n)]
 arr.sort(key=lambda x: (x[1],x[0]))
 names = [i[0] for i in arr]
 marks = [i[1] for i in arr]
